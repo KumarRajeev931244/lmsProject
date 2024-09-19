dc43a451a8aa83bd6b048622a9a527d0eb1b9165
KumarRajeev931244 <rajeevkumar25112002@gmail.com>
2024-09-19 18:29:49 +0530

    basic setup auth

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,17 @@ import App from './App.jsx'
 
 // css import
 import './index.css'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import store from './Redux/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        <Toaster/>
+        </BrowserRouter>
+    </Provider>
  
 )
